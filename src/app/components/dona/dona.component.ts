859625d3e834d97301706e6910465dbb8224b386
max_importa@hotmail.com <max_importa@hotmail.com>
2023-10-31 19:28:59 +0100

    fin seccion 6

--- src/app/components/dona/dona.component.ts
+++ src/app/components/dona/dona.component.ts
@@ -1,53 +1,32 @@
 import { Component, Input } from '@angular/core';
-import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
+import { ChartEvent, ChartType, ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class DonaComponent {
 
-  @Input() title: string = 'Sin titulo';
+  @Input() title: string = 'Sin título';
+  @Input('labels') doughnutChartLabels: string[] = ['Label', 'Label', 'Label3'];
+  @Input('data') doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [350, 450, 100],
+        backgroundColor: ['#9E120E', '#FF58800', '#FFB414'],
+      },
+    ],
+  };
 
-  
-    // Doughnut
-    public doughnutChartLabels: string[] = [
-      'Download Sales',
-      'In-Store Sales',
-      'Mail-Order Sales',
-    ];
-    public doughnutChartData: ChartData<'doughnut'> = {
-      labels: this.doughnutChartLabels,
-      datasets: [
-        { data: [350, 450, 100], 
-          backgroundColor:['#9E120E','#FF58800','#FFB414']},
-        // { data: [50, 150, 120] },
-        // { data: [250, 130, 70] },
-      ],
-    };
-    public doughnutChartType: ChartType = 'doughnut';
-  
-    // events
-    public chartClicked({
-      event,
-      active,
-    }: {
-      event: ChartEvent;
-      active: object[];
-    }): void {
-      console.log(event, active);
-    }
-  
-    public chartHovered({
-      event,
-      active,
-    }: {
-      event: ChartEvent;
-      active: object[];
-    }): void {
-      console.log(event, active);
-    }
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public chartClicked(event: ChartEvent, active: object[]): void {
+    console.log(event, active);
   }
 
+  public chartHovered(event: ChartEvent, active: object[]): void {
+    console.log(event, active);
+  }
+}
--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -10,7 +10,13 @@ import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
 })
 export class Grafica1Component {
   public labels1: string[] = ['Pan', 'Refresco', 'Tacos'];
-  public data1 = [
-    [10, 15, 40],
-  ];
+  public data1: ChartData<'doughnut'> = {
+    labels: this.labels1,
+    datasets: [
+      {
+        data: [50,20, 10],
+        backgroundColor: ['#9E120E', '#FF58800', '#FFB414'],
+      },
+    ],
+  };
 }
